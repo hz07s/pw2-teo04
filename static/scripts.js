075db238a62900e988dca7e8be1e30f9043d966b
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-05-20 20:01:02 -0500

    agregando funcion para actualizar campos de opciones segun la tabla que se seleccione

--- static/scripts.js
+++ static/scripts.js
@@ -6,3 +6,26 @@ document.getElementById('formulario-busqueda').addEventListener('submit', functi
 document.getElementById('tabla').addEventListener('change', function() {
     actualizarCampoOptions();
 });
+
+function actualizarCampoOptions() {
+    var tablaSeleccionada = document.getElementById('tabla').value;
+    var campoSelect = document.getElementById('campo');
+
+    campoSelect.innerHTML = '';
+
+    var options;
+    if (tablaSeleccionada === 'Movie') {
+        options = ['MovieID', 'Title', 'Year', 'Score', 'Votes'];
+    } else if (tablaSeleccionada === 'Actor') {
+        options = ['ActorId', 'Name'];
+    } else if (tablaSeleccionada === 'Casting') {
+        options = ['MovieID', 'ActorId', 'Ordinal'];
+    }
+
+    options.forEach(optionValue => {
+        var option = document.createElement('option');
+        option.value = optionValue;
+        option.textContent = optionValue;
+        campoSelect.appendChild(option);
+    });
+}
