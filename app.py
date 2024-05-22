fb61ddcacb558bbb42d02e0ae9fee07f9e768b39
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-05-21 21:04:02 -0500

    se añadio la ruta para manejar la busqueda y recibir datos del formulario

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/buscar', methods=['POST'])
+def buscar():
+    tabla = request.form['tabla']
+    campo = request.form['campo']
+    valor = request.form['valor']
+    return jsonify({'tabla': tabla, 'campo': campo, 'valor': valor})
 
 if __name__ == '__main__':
     app.run(debug=True)
